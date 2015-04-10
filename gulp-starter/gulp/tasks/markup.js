af29000b69b1e010c4a06e0ec77ae6dceaea8b39
mjbp <mickperkins@gmail.com>
2015-04-10 13:17:22 +0100

    Removed unused dev files

--- gulp-starter/gulp/tasks/markup.js
+++
@@ -1,9 +0,0 @@
-var gulp       = require('gulp');
-var config     = require('../config').markup;
-var jade       = require('gulp-jade');
-
-gulp.task('markup', function () {
-  return gulp.src(config.src + '*.jade')
-    .pipe(jade())
-    .pipe(gulp.dest(config.dest));
-});
